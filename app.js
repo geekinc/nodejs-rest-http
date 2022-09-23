89e52a4768c6096c258efd48323911076c2e0ed0
Ben Inkster <ben@geekinc.ca>
2022-09-23 13:22:37 -0600

    Simple test for build

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/api/greeting', (request, response) => {
   const name = request.query ? request.query.name : undefined;
-  response.send({ content: `Hello, ${name || 'World!'}` });
+  response.send({ content: `Hi there, ${name || 'World!'}` });
 });
 
 module.exports = app;
